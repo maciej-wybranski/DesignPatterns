334e64f8dd775b0a037e9174c165bce7975cc785
Maciej <wybranski.maciej@outlook.com>
2019-09-26 07:33:05 +0200

    Placing in the try block the code responsible for creating the square (v2)

--- DesignPaterns.Adapter/Program.cs
+++ DesignPaterns.Adapter/Program.cs
@@ -14,11 +14,18 @@ namespace DesignPaterns.Adapter
             IRegularPolygon square = new Square(squareCenter, 5.0f);
             Client.DisplayShapeInformation(square);
 
+            try
+            {
+                //Rectangle rectangle = new Rectangle(new Point(10.0f, 10.0f), new Point(20.0f, 30.0f)); //this is not regular rectangle, we get ArgumentException
+                Rectangle rectangle = new Rectangle(new Point(10.0f, 10.0f), new Point(20.0f, 20.0f));
+                IRegularPolygon square2 = new Square2(rectangle);
+                Client.DisplayShapeInformation(square2);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
             
-            //Rectangle rectangle = new Rectangle(new Point(10.0f, 10.0f), new Point(20.0f, 30.0f)); //this is not regular rectangle, we get ArgumentException
-            Rectangle rectangle = new Rectangle(new Point(10.0f, 10.0f), new Point(20.0f, 20.0f));
-            IRegularPolygon square2 = new Square2(rectangle);
-            Client.DisplayShapeInformation(square2);
 
         }
     }
