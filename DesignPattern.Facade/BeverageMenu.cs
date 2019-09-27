d4d6abc6cd8fb6f257ca5ac36639009ea04a6152
Maciej <wybranski.maciej@outlook.com>
2019-09-27 11:43:36 +0200

    Created facade example

---
+++ DesignPattern.Facade/BeverageMenu.cs
@@ -0,0 +1,32 @@
+﻿using DesignPattern.Decorator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Facade
+{
+    static class BeverageMenu
+    {
+        public static Beverage CoffeeWithMilk()
+        {
+            Beverage beverage = new Coffee();
+            beverage = new BeverageWithMilk(beverage);
+            return beverage;
+        }
+
+        public static Beverage CoffeeWithMilkAndSuggar()
+        {
+            Beverage beverage = new Coffee();
+            beverage = new BeverageWithSuggar(beverage);
+            beverage = new BeverageWithMilk(beverage);
+            return beverage;
+        }
+
+        public static Beverage TeaWithSuggar(bool brownSuggar = false)
+        {
+            Beverage beverage = new Tea();
+            beverage = new BeverageWithSuggar(beverage, brownSuggar);
+            return beverage;
+        }
+    }
+}
---
+++ DesignPattern.Facade/Program.cs
@@ -0,0 +1,27 @@
+﻿using DesignPattern.Decorator;
+using System;
+
+namespace DesignPattern.Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Beverage coffee = new Coffee();
+            Console.WriteLine($"{coffee}: {coffee.Price()} zł");
+            Console.WriteLine($"Pojemność: {coffee.Size()} ml.");
+
+            Beverage coffeWithMilk = BeverageMenu.CoffeeWithMilk();
+            Console.WriteLine($"{coffeWithMilk}: {coffeWithMilk.Price()} zł");
+            Console.WriteLine($"Pojemność: {coffeWithMilk.Size()} ml.");
+
+            Beverage coffeWithMilkAndSuggar = BeverageMenu.CoffeeWithMilkAndSuggar();
+            Console.WriteLine($"{coffeWithMilkAndSuggar}: {coffeWithMilkAndSuggar.Price()} zł");
+            Console.WriteLine($"Pojemność: {coffeWithMilkAndSuggar.Size()} ml.");
+
+            Beverage teaWithSuggar = BeverageMenu.TeaWithSuggar(true);
+            Console.WriteLine($"{teaWithSuggar}: {teaWithSuggar.Price()} zł");
+            Console.WriteLine($"Pojemność: {teaWithSuggar.Size()} ml.");
+        }
+    }
+}
