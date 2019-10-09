b6dddebe0d65a3b247b7b286fae0b800a9ea663f
Maciej <wybranski.maciej@outlook.com>
2019-10-09 21:43:46 +0200

    Proxy design pattern example (it's using reference to bridge example)

---
+++ DesignPattern.Proxy/MultiSender.cs
@@ -0,0 +1,29 @@
+﻿using DesignPattern.Bridge;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Proxy
+{
+    class MultiSender : IMessageSender
+    {
+        private HashSet<IMessageSender> _messageSenders;
+
+        public void SendMessage(string subject, string body)
+        {
+            if (_messageSenders == null)
+            {
+                _messageSenders = new HashSet<IMessageSender>
+                {
+                    new EmailSender(),
+                    new SmsSender(),
+                    new WebServiceSender()
+                };
+            }
+            foreach(var messageSender in _messageSenders)
+            {
+                messageSender.SendMessage(subject, body);
+            }
+        }
+    }
+}
---
+++ DesignPattern.Proxy/Program.cs
@@ -0,0 +1,22 @@
+﻿using DesignPattern.Bridge;
+using System;
+
+namespace DesignPattern.Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message message = new UserMessage()
+            {
+                Subject = "Proxy design pattern",
+                Body = "I'm testing the proxy design pattern",
+                UserComments = "Is it working? .... I hope so."
+            };
+
+            IMessageSender messageSender = new MultiSender();
+            message.MessageSender = messageSender;
+            message.Send();
+        }
+    }
+}
